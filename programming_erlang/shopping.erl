43b0154fbd959894da969cd7fdc8e8392cf0dea0
David Rouchy <drouchy@gmail.com>
2013-08-17 10:41:31 +0200

    shopping

---
+++ programming_erlang/shopping.erl
@@ -0,0 +1,13 @@
+-module(shopping).
+-export([cost/1, default_basket/0, total/1]).
+
+default_basket() -> [{oranges,4}, {newspaper,1}, {apples, 10}, {pears,6}, {milk,3}].
+
+cost(oranges)    -> 5;
+cost(newspaper) -> 8;
+cost(apples)    -> 2;
+cost(pears)     -> 9;
+cost(milk)      -> 7.
+
+total([])              -> 0;
+total([{What, N} | T]) -> cost(What) * N + total(T).
